1dfae8d43ca327feaad7724fc4ffc149ebdbd65d
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 20:48:17 +0300

    Initial commit

---
+++ NumbersWithThreeSteps/Program.cs
@@ -0,0 +1,12 @@
+﻿class NumbersWithThreeSteps {
+    public static void Main (String[]args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i=1;i<=n;i+=3) {
+            Console.WriteLine(i);
+
+        }
+    }
+}
